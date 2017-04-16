c7ce6060501819aed90964cbee64385bb5c724eb
Swarts, Matthew E <strontium5@gmail.com>
2017-04-15 21:42:32 -0400

    forgot to add the serialEvent function to capture serial events for the GOLStationSerialEventTest

--- firmware/GOLStationSerialEventTest/GOLStationSerialEventTest.ino
+++ firmware/GOLStationSerialEventTest/GOLStationSerialEventTest.ino
@@ -138,3 +138,23 @@ uint32_t Wheel(byte WheelPos) {
   WheelPos -= 170;
   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
+
+/*
+  SerialEvent occurs whenever a new data comes in the
+ hardware serial RX.  This routine is run between each
+ time loop() runs, so using delay inside loop can delay
+ response.  Multiple bytes of data may be available.
+ */
+void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    inputString += inChar;
+    // if the incoming character is a newline, set a flag
+    // so the main loop can do something about it:
+    if (inChar == '\n') {
+      stringComplete = true;
+    }
+  }
+}
