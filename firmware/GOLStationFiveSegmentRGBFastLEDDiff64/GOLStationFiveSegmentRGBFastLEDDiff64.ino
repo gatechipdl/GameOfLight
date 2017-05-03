a20dddc4cba962dc7c06bafd41a47fddcab59604
Swarts, Matthew E <strontium5@gmail.com>
2017-05-03 08:01:11 -0400

    integrated a diff and dediff differencing encoding scheme to deal with zeros in the uart byte array

---
+++ firmware/GOLStationFiveSegmentRGBFastLEDDiff64/GOLStationFiveSegmentRGBFastLEDDiff64.ino
@@ -0,0 +1,176 @@
+// Protocol Byte Sequence:
+
+// Station ID 0:
+// 0_R1, 0_G1, 0_B1 LOWER SEGMENT
+// 0_R2, 0_G2, 0_B2 LOWER MIDDLE SEGMENT
+// 0_R3, 0_G3, 0_B3 MIDDLE SEGMENT
+// 0_R4, 0_G4, 0_B4 UPPER MIDDLE SEGMENT
+// 0_R5, 0_G5, 0_B5 UPPER SEGMENT
+
+// Station ID 1:
+// 1_R1, 1_G1, 1_B1 LOWER SEGMENT
+// 1_R2, 1_G2, 1_B2 LOWER MIDDLE SEGMENT
+// 1_R3, 1_G3, 1_B3 MIDDLE SEGMENT
+// 1_R4, 1_G4, 1_B4 UPPER MIDDLE SEGMENT
+// 1_R5, 1_G5, 1_B5 UPPER SEGMENT
+
+// ...
+// ...
+// ...
+
+// Station ID 35:
+// 35_R1, 35_G1, 35_B1 LOWER SEGMENT
+// 35_R2, 35_G2, 35_B2 LOWER MIDDLE SEGMENT
+// 35_R3, 35_G3, 35_B3 MIDDLE SEGMENT
+// 35_R4, 35_G4, 35_B4 UPPER MIDDLE SEGMENT
+// 35_R5, 35_G5, 35_B5 UPPER SEGMENT
+
+#include <ESP8266WiFi.h>
+
+#define STATION_ID 24
+//possible to go up to 250000 with ~20ms refresh rate
+#define SERIAL_BAUD 115200
+
+#include "FastLED.h"
+
+FASTLED_USING_NAMESPACE
+
+#if defined(FASTLED_VERSION) && (FASTLED_VERSION < 3001000)
+#warning "Requires FastLED 3.1 or later; check github for latest code."
+#endif
+
+#define DATA_PIN    4
+//#define CLK_PIN   4
+#define LED_TYPE    WS2811
+#define COLOR_ORDER GRB
+#define NUM_LEDS   45
+CRGB leds[NUM_LEDS];
+uint8_t max_brightness = 255; 
+
+#define PIN_RS485_RECEIVE 13
+
+#define STATION_LED_COUNT 45
+#define STATION_SEGMENT_COUNT 5
+#define STATION_LED_SEGMENT_COUNT 9
+
+//3 bytes of color, 5 segments, 36 stations = 540
+#define COLOR_BYTE_COUNT 3
+#define STATION_BYTE_COUNT 540
+#define MAX_MILLIS_TO_WAIT_FOR_PACKET 30
+
+byte diff64[] = {
+(byte)0x65, (byte)0x14, (byte)0xe7, (byte)0xdf, (byte)0x4c, (byte)0xe0, (byte)0x6b, (byte)0xb4,
+(byte)0x9f, (byte)0xe4, (byte)0x06, (byte)0x4d, (byte)0x2c, (byte)0x85, (byte)0x32, (byte)0xfe,
+(byte)0xc6, (byte)0x38, (byte)0x4f, (byte)0x19, (byte)0xa7, (byte)0x78, (byte)0x29, (byte)0x3f,
+(byte)0xf5, (byte)0x5b, (byte)0x6c, (byte)0x03, (byte)0x6a, (byte)0x8b, (byte)0x8b, (byte)0xf0, 
+(byte)0x96, (byte)0xa8, (byte)0x3d, (byte)0xad, (byte)0x3b, (byte)0x8f, (byte)0xbc, (byte)0xc0,
+(byte)0x58, (byte)0x51, (byte)0xf7, (byte)0x66, (byte)0x26, (byte)0xbb, (byte)0x5a, (byte)0xb5,
+(byte)0x16, (byte)0x20, (byte)0x14, (byte)0x10, (byte)0x69, (byte)0x72, (byte)0x75, (byte)0xc3,
+(byte)0x31, (byte)0xcc, (byte)0x3b, (byte)0x01, (byte)0xb7, (byte)0x2b, (byte)0xf4, (byte)0x71
+};
+
+uint32_t lastSerialEventMillis;
+boolean packetComplete = false;  // whether the packet is complete
+byte packetBytesSerialBuffer[STATION_BYTE_COUNT];
+byte packetBytes[STATION_BYTE_COUNT];
+uint16_t packetBytesReceivedCount = 0;
+
+CRGB stationColors[STATION_SEGMENT_COUNT];
+uint8_t stationId = STATION_ID;
+
+boolean doPrepareStationSegmentColors = false;
+uint8_t stationLedColorIndex[STATION_LED_COUNT];
+uint8_t currentLedIndex = 0;
+boolean doShowStrip = false;
+ 
+uint8_t gHue = 0; // rotating "base color" used by many of the patterns
+CRGB rgbval(50,0,250);
+
+void setup() {
+  delay(2000); // 2 second delay for recovery
+  
+  // tell FastLED about the LED strip configuration
+  FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  //FastLED.addLeds<LED_TYPE,DATA_PIN,CLK_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+
+  // set master brightness control
+  FastLED.setBrightness(max_brightness);
+  
+  pinMode(PIN_RS485_RECEIVE,OUTPUT);
+  digitalWrite(PIN_RS485_RECEIVE,LOW); //listen mode
+  
+  lastSerialEventMillis = millis();
+  
+//  //index once
+//  stationLedColorIndex[0]=0; //TODO integrate black pixel as first pixel
+//  for(uint8_t i=0;i<STATION_SEGMENT_COUNT;i++){
+//    for(uint8_t j=0;j<STATION_LED_SEGMENT_COUNT;j++){
+//      stationLedColorIndex[i*STATION_LED_SEGMENT_COUNT+j+1]=i;
+//    }
+//  }
+  
+  // initialize serial:
+  Serial.begin(SERIAL_BAUD);
+
+  // turn off wifi
+  WiFi.mode(WIFI_OFF);
+}
+
+void loop() {
+  checkSerialEvent(); //check the serial event buffer
+  
+  if (packetComplete) {
+    packetBytesReceivedCount = 0; //reset the packet bytes received count
+    //lastSerialEventMillis = millis(); //reset timer
+    memcpy(packetBytes, packetBytesSerialBuffer, STATION_BYTE_COUNT);
+    
+    //rgb1,rgb2,rgb3,rgb4,rgb5
+    for(uint8_t i=0; i<STATION_SEGMENT_COUNT;i++){
+      stationColors[i] = CRGB(
+        (uint8_t)packetBytes[((stationId*STATION_SEGMENT_COUNT+i)*COLOR_BYTE_COUNT+2)], //BLUE
+        (uint8_t)packetBytes[((stationId*STATION_SEGMENT_COUNT+i)*COLOR_BYTE_COUNT+1)], //GREEN
+        (uint8_t)packetBytes[((stationId*STATION_SEGMENT_COUNT+i)*COLOR_BYTE_COUNT+0)]  //RED
+      );
+    }
+    
+    fill_solid(leds+0,10,stationColors[0]);
+    fill_solid(leds+10,9,stationColors[1]);
+    fill_solid(leds+19,9,stationColors[2]);
+    fill_solid(leds+28,9,stationColors[3]);
+    fill_solid(leds+37,8,stationColors[4]);
+    FastLED.show();
+    
+    packetComplete = false;
+  }
+}
+
+/*
+  SerialEvent occurs whenever a new data comes in the
+ hardware serial RX.  This routine is run between each
+ time loop() runs, so using delay inside loop can delay
+ response.  Multiple bytes of data may be available.
+ */
+void checkSerialEvent() {
+  //bool hadData = false;
+  while (Serial.available()) {
+    
+    // get the next new byte, and "de-diff"
+    packetBytesSerialBuffer[packetBytesReceivedCount] = (byte)(Serial.read()-diff64[packetBytesReceivedCount%64]);
+    packetBytesReceivedCount++;
+    if (packetBytesReceivedCount == STATION_BYTE_COUNT) {
+      packetComplete = true; //flag the packet to be processed
+      Serial.flush();
+    }
+    //hadData = true;
+  }
+//  if(hadData){
+//    lastSerialEventMillis = millis();
+//  }
+//
+//  //reset the packetBytes count if there hasn't been bytes in over 1 second
+//  if( millis() - lastSerialEventMillis > MAX_MILLIS_TO_WAIT_FOR_PACKET){
+//    packetBytesReceivedCount = 0; //reset the packet byte count
+//    Serial.flush(); //clear out anything that might be in the serial buffer
+//    lastSerialEventMillis = millis(); //reset timer
+//  }
+}
---
+++ processing sketches/GOLStationFiveSegmentRGB_Diff64/GOLStationFiveSegmentRGB_Diff64.pde
@@ -0,0 +1,126 @@
+import processing.serial.*;
+
+Serial sPort;  // Create object from Serial class
+
+private static final int SERIAL_BAUD = 115200;
+//3 bytes of color, 5 segments, 36 stations = 540
+private static final int STATION_COUNT = 36;
+private static final int STATION_LED_COUNT = 45;
+private static final int STATION_SEGMENT_COUNT = 5;
+private static final int STATION_LED_SEGMENT_COUNT = 9;
+private static final int COLOR_BYTE_COUNT = 3;
+private static final int STATION_COLOR_COUNT = 180;
+private static final int STATION_BYTE_COUNT = 540;
+private static final int BASE_CYCLE_MILLIS = 10000;
+
+int canvasWidth = 600;
+int canvasHeight = 600;
+
+byte[] packetBytes = new byte[STATION_BYTE_COUNT];
+byte[] packetBytesDiff = new byte[STATION_BYTE_COUNT];
+color[] colors = new color[STATION_COLOR_COUNT];
+float[] hueIndices = new float[STATION_SEGMENT_COUNT];
+
+byte[] diff64 = {
+(byte)0x65, (byte)0x14, (byte)0xe7, (byte)0xdf, (byte)0x4c, (byte)0xe0, (byte)0x6b, (byte)0xb4,
+(byte)0x9f, (byte)0xe4, (byte)0x06, (byte)0x4d, (byte)0x2c, (byte)0x85, (byte)0x32, (byte)0xfe,
+(byte)0xc6, (byte)0x38, (byte)0x4f, (byte)0x19, (byte)0xa7, (byte)0x78, (byte)0x29, (byte)0x3f,
+(byte)0xf5, (byte)0x5b, (byte)0x6c, (byte)0x03, (byte)0x6a, (byte)0x8b, (byte)0x8b, (byte)0xf0, 
+(byte)0x96, (byte)0xa8, (byte)0x3d, (byte)0xad, (byte)0x3b, (byte)0x8f, (byte)0xbc, (byte)0xc0,
+(byte)0x58, (byte)0x51, (byte)0xf7, (byte)0x66, (byte)0x26, (byte)0xbb, (byte)0x5a, (byte)0xb5,
+(byte)0x16, (byte)0x20, (byte)0x14, (byte)0x10, (byte)0x69, (byte)0x72, (byte)0x75, (byte)0xc3,
+(byte)0x31, (byte)0xcc, (byte)0x3b, (byte)0x01, (byte)0xb7, (byte)0x2b, (byte)0xf4, (byte)0x71
+};
+
+
+void setup() 
+{
+  size(600, 600);
+  canvasWidth = width;
+  canvasHeight = height;
+  
+  byte[] test = {0,0,0,0};
+  println(test);
+  byte[] test2 = new byte[test.length];
+  //System.arraycopy(test,0,test2,0,test.length);
+  for(int i=0;i<test.length;i++){
+    test2[i] = (byte)(test[i] + diff64[i%64] + diff64[i%64] + diff64[i%64]);
+  }
+  println(test2);
+  
+  byte[] test3 = new byte[test.length];
+  for(int i=0;i<test.length;i++){
+    test3[i] = (byte)(test2[i] - diff64[i%64] - diff64[i%64] - diff64[i%64]);
+  }
+  println(test3);
+  
+  
+  
+  // List all the available serial ports:
+  printArray(Serial.list());
+  
+  String portName = Serial.list()[0];
+  sPort = new Serial(this, portName, SERIAL_BAUD);
+  
+  colorMode(HSB,1f);
+  updateHuesAndBytes();
+}
+
+void draw() {
+  delay(10);
+  background(255); //clear the background
+  updateHuesAndBytes();
+  
+  for(int i=0;i<6;i++){
+    for(int j=0;j<6;j++){
+      for(int k=0;k<5;k++){
+        int colorIndex = (i*6+j)*5+k;
+        fill(colors[colorIndex]);
+        
+        rect(canvasWidth/6*j,canvasHeight/6*i+canvasHeight/30*(5-k-1),canvasWidth/6,canvasHeight/30);
+      }
+    }
+  }
+  
+  // "diff" encode the packetBytes
+  for(int i=0;i<STATION_BYTE_COUNT;i++){
+    packetBytesDiff[i] = (byte)(packetBytes[i] + diff64[i%64]);
+  }
+  sPort.write(packetBytesDiff);
+  //printArray(packetBytes);
+  //for(int i=0;i<STATION_BYTE_COUNT/4;i++){
+  //  sPort.write(packetBytes[i]);
+  //  //delay(1);
+  //}
+}
+
+void updateHuesAndBytes(){
+  int m = millis();
+  for(int i=0;i<STATION_SEGMENT_COUNT;i++){
+    hueIndices[i] = 1f*(m%(BASE_CYCLE_MILLIS*(i+1)))/float(BASE_CYCLE_MILLIS*(i+1));
+  }
+  
+  //AAAAAAAARRRRRRRRGGGGGGGGBBBBBBBB
+  for(int s=0;s<STATION_COUNT;s++){
+    for(int i=0;i<STATION_SEGMENT_COUNT;i++){
+      float hueIndex = (hueIndices[0] + s/float(STATION_COUNT*5));
+      hueIndex -= floor(hueIndex);
+      //float hueIndex = (i/float(STATION_SEGMENT_COUNT));
+      //float hueIndex = 0.7f;
+      color c = color( hueIndex,1f,0.02f);
+      //c = color( 0f,1f,1f); //just a solid color
+      //println(hueIndex+":"+int(red(c)*255)+","+int(green(c)*255)+","+int(blue(c)*255));
+      int colorIndex = s*STATION_SEGMENT_COUNT+i;
+      //c = color( random(0.0,1.0),1f,1f); //random test
+      colors[colorIndex] = c;
+      packetBytes[colorIndex*COLOR_BYTE_COUNT+0] = (byte)((c >> 16) & 255); //RED
+      packetBytes[colorIndex*COLOR_BYTE_COUNT+1] = (byte)((c >>  8) & 255); //GREEN
+      packetBytes[colorIndex*COLOR_BYTE_COUNT+2] = (byte)((c >>  0) & 255); //BLUE
+      
+      
+      packetBytes[colorIndex*COLOR_BYTE_COUNT+0] = byte(red(c)*255);
+      packetBytes[colorIndex*COLOR_BYTE_COUNT+1] = byte(green(c)*255);
+      packetBytes[colorIndex*COLOR_BYTE_COUNT+2] = byte(blue(c)*255);
+    }
+  }
+}
