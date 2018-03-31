1bbb86b8fd512b5ad3718740db2fdd15c35f5e44
clementzheng <clement.zheng@gmail.com>
2018-03-31 14:49:34 -0600

    fixed bug

--- server/public/js/roms/rainbow.js
+++ server/public/js/roms/rainbow.js
@@ -1,4 +1,4 @@
-var rainbow_cap_delta = 2000;
+var rainbow_cap_delta = 3000;
 var rainbow_animation_interval = 250;
 
 rom_list.push(
@@ -75,17 +75,15 @@ function rainbowAnimation() {
                     var h = (rainbow_h_counter+1-0.2*i-0.04*j)%1;
                     var s = 0;
                         if (top_touch_bool) {
-                            s = top_touch_val+0.5-(rainbow_s_counter+0.1*k)%0.5;  
+                            s = top_touch_val+0.5-(rainbow_s_counter[i][j]+0.1*k)%0.5;  
                         } else {
                             s = top_touch_val+0.5-(0.1*k)%0.5; 
                         }
-                    
                             
 
                     setLayerColorHSV(i, j, k, h, s, undefined); //starts off completely dark with full saturation at about the hue blue
                 }
                     
-                rainbow_s_counter[i][j] = rainbow_s_counter[i][j] + rainbow_s_incr;
                 rainbow_s_counter[i][j] = rainbow_s_counter[i][j] + rainbow_s_incr;
             }
         }
@@ -99,11 +97,19 @@ function rainbowAnimation() {
 
 function rainbowColors() {
     rainbow_run = !rainbow_run;
-    if (rainbow_run == false){
+    if (rainbow_run == false) {
         for (var i=0; i<LO_config.rows; i++) {
             for (var j=0; j<LO_config.cols; j++) {
                 for (var k=0; k<LO_config.layers; k++) {
                     setLayerColorHSV(i, j, k, 0.5, 1.0, 0.0); //starts off completely dark with full saturation at about the hue blue
+                }
+            }
+        }
+    } else {
+        for (var i=0; i<LO_config.rows; i++) {
+            for (var j=0; j<LO_config.cols; j++) {
+                for (var k=0; k<LO_config.layers; k++) {
+                    setLayerColorHSV(i, j, k, undefined, undefined, 1.0); //starts off completely dark with full saturation at about the hue blue
                 }
             }
         }
